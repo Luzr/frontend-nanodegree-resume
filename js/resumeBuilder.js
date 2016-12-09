1fbe1f59daed56f60d6daab3756336352d3e2530
Anders R <airiedemann@gmail.com>
2016-12-09 11:13:21 -0600

    Having Issues
    Issues with anything showing.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,93 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var work = {
+    "jobs": [
+        {
+            "employer": "IT Stability",
+            "title": "Sysadmin",
+            "location": "Elgin",
+            "dates": "2008-2016",
+            "description": " Moved servers around elit. Ut a ullamcorper quam, vel aliquet dolor. Morbi fringilla tellus metus, convallis vulputate dui auctor vitae. Ut sagittis turpis varius purus tincidunt, quis blandit justo placerat. Nullam turpis metus, porta sit amet arcu quis, egestas viverra lorem. Nullam volutpat suscipit enim, ac rhoncus tellus pulvinar a. Morbi lobortis, diam molestie lobortis ultricies, quam magna cursus quam, eget facilisis erat leo nec ligula. Sed eu nunc velit. Suspendisse mi arcu, scelerisque nec purus convallis, lobortis semper ante. Nullam lobortis pellentesque sem dictum congue. Nulla quis justo non mi tempus interdum. Donec consectetur vitae nisi a molestie. Aliquam finibus efficitur bibendum. Etiam imperdiet justo nisl, vel vehicula augue scelerisque at."
+        },
+        {
+            "employer": "AdNET",
+            "title": "Lvel 1 Tech",
+            "location": "Elgin",
+            "dates": "2004-2008",
+            "description": "Installed computers ut porttitor ex ac ullamcorper suscipit. Aliquam in magna quis quam egestas dignissim. Sed et dolor iaculis, maximus sapien vitae, pellentesque enim. Curabitur eget vulputate quam, id congue ante. Vivamus quis neque sodales, finibus ante sed, congue arcu. Nam venenatis orci in tincidunt ultricies. Ut libero metus, fringilla ac placerat ac, accumsan id erat. Donec ut molestie libero. Nullam non ligula neque. Vestibulum eu velit nec lacus blandit volutpat"
+        },
+    ]
+}
+
+var projects = {
+    "projects": [
+        {
+        "title": "Install Server",
+        "date": "2009",
+        "description": "Install a server to work on. Quisque eget dignissim metus. Aenean eget dictum lorem, at elementum magna. Ut vulputate felis id fringilla tristique. Sed aliquet leo vulputate nisl tincidunt volutpat. Quisque nec ornare purus, sed sagittis eros in."
+        }
+    ]    
+}
+
+var bio = {
+    "name": "Anders Riedemann",
+    "role": "Roleing",
+    "welcomemessage": "Thanks for looking at my resume",
+    "biopic": "images/me.jpg",
+    "contacts":{
+        "mobile": "115-337-4164",
+        "email": "anders@airideas.net",
+        "github": "airiedemann",
+        "twitter": "@airiedemann",
+        "location": "Chicago"
+    }
+}
+
+
+
+var education = {
+    "schools": [
+        {
+            "name": "Elgin Community College",
+            "location": "Elgin",
+            "degree": "AS",
+            "major": ["Network","System Admistration"],
+            "dates": "2004"
+            
+        },
+        {
+            "name": "northern illinois university",
+            "city": "DeKalb",
+            "degree": "BA",
+            "major":["Network Security"]
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title": "Nanodegrees",
+            "school": "Udacity",
+            "dates": "2016",
+            "url": "Udacity.com"
+        }
+    ]
+}
+
+for (job in work.jobs) {
+    $("WorkExperience").append(HTMLworkStart);
+
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+    var formattedTitle = HTMLworkTitle.replace("%data$",work.jobs[job].title);
+    var formattedEmployerTitle = formattedEmployerTitle + formattedTitle;
+    
+    $(".work-entry:last").append(formattedEmployerTitle);
+
+    var formattedDates = HTMLworkDates.replace("%data$", work.jobs[job].dates);
+    $(".work-entry:last").append(formattedDates)
+
+    var formatttedDescription = HTMLworkDescription.replace("%data$", work.jobs[job].description);
+
+    $(".work-entry:last").append(formatttedDescription);
+
+}
---
+++ random.js
@@ -0,0 +1,85 @@
+// $("#main").append("Anders Riedemann");
+
+// var firstname = "Anders";
+// var age = 31;
+
+// console.log(firstname)
+
+// var awesomeThoughts ="My name is Anders and I am AWESOME";
+
+// var funThoughts = awesomeThoughts.replace("AWESOME!","FUN")
+
+// $("#main").append(funThoughts);
+
+// var name = "Anders Riedemann";
+// var role = "Web Developer";
+
+// var formattedName = HTMLheaderName.replace("%data%", name);
+
+// var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+
+// $("#header").prepend(formattedName);
+// $("#header").prepend(formattedRole);
+// var bio = {
+//     "name": "Anders Riedmeann",
+//     "role": "Web Developer",
+//     "contact": {
+//         "mobile": "312-124-1486",
+//         "email": "anders@airideas.net",
+//         "github": "anders",
+//         "twitter": "@theCat"
+//     },
+//     "bio": "I like coffee",
+//     "pictureurl": "images/me.jpg",
+//     "welcomemessage": "Thanks for looking.",
+//     "skills": ["driving","walking","sleeping"]
+// }
+
+// var formattedName = HTMLheaderName.replace("%data%", bio.name);
+
+// var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
+// var formattedMobile = HTMLmobile.replace("%data%", bio.contact.mobile);
+// var formattedLEmail = HTMLemail.replace("%data%",bio.contact.email);
+// var formattedLTwitter = HTMLtwitter.replace("%data%",bio.contact.twitter);
+// var formattedLGithub = HTMLgithub.replace("%data%",bio.contact.github);
+// var formattedLBioPic = HTMLbioPic.replace("%data%",bio.pictureurl);
+// var formattedLWelcomeMsg = HTMLwelcomeMsg.replace("%data%",bio.welcomemessage);
+// //var formattedLSkillsStart = HTMLskillsStart
+// var formattedLHTMLskills = HTMLskills.replace("%data%",bio.skills[1]);
+
+// var work = {};
+// work.position = "Sysadmin";
+// work.employer = "AirIdeas";
+// work.years = 14;
+// work.city = "Chicago";
+
+// var eduaction = {};
+// eduaction["name"] = "School of Hard Knocks";
+// eduaction["years"] = "2001-2013";
+// eduaction["city"]= "New York, New York";
+
+
+// var formattedWorkEmployer = HTMLworkEmployer.replace("%data%",work.employer);
+// var formattedWorkPosition = HTMLworkEmployer.replace("%data%",work.position);
+// var formattedWorkYears = HTMLworkEmployer.replace("%data%",work.years);
+// var formattedWorkCity = HTMLworkEmployer.replace("%data%",work.city);
+
+
+// $("#header").prepend(formattedName);
+// $("#header").prepend(formattedRole);
+// $("#header").append(formattedLEmail);
+// $("#header").append(formattedLTwitter);
+// $("#header").append(formattedLGithub);
+// $("#header").append(formattedLBioPic);
+// $("#header").append(formattedLWelcomeMsg);
+// // $("#header").append(formattedLSkillsStart);
+// $("#header").append(formattedLHTMLskills);
+
+// $("#main").append(formattedWorkEmployer);
+// $("#main").append(formattedWorkPosition);
+// $("#main").append(formattedWorkYears);
+// $("#main").append(formattedWorkCity);
+// $("#main").append(eduaction["years"]);
+
+// $("main").append(eduaction["name"]);
